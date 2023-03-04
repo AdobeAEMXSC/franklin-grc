7a58ed4d0b1eaffd7626bd6281f39fc6f1cfdd33
Kyle Chau <kchau@adobe.com>
2023-03-04 09:36:19 -0500

    updated css

--- blocks/background-image/background-image.js
+++ blocks/background-image/background-image.js
@@ -1,12 +1,21 @@
-
 var bgImages = [];
 var currentImage = 0;
 
-function nextImage() {
+// change body CSS for background image block
+document.body.style.height = "100%"; 
+document.body.style.width = "auto";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundPosition = "center center";
+document.body.style.backgroundAttachment = "fixed";
+document.body.style.backgroundRepeat = "no-repeat";  
+document.body.style.overflow = "hidden";
+
+function nextImage() {    
     if (currentImage == bgImages.length) currentImage = 0;
     document.body.style.backgroundImage = "url('" + bgImages[currentImage] + "')";
     currentImage++;
 }
+
 
 export default function decorate(block) {
     block.style.display = "none";
@@ -15,5 +24,7 @@ export default function decorate(block) {
     });
 
     nextImage();
-    const interval = setInterval(function() {nextImage()}, 3000);
+    const interval = setInterval(function() {nextImage()}, 3000);    
 }
+
+//document.getElementsByClassName("footer-wrapper")[0].style.dispay = "none";
